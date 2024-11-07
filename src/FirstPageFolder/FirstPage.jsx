130f8653b5579e2a91c6920986e148762fd5cc7c
Sahil <sahilvunnam423@gmail.com>
2024-11-06 18:33:00 -0800

    SahilsMerge

--- src/FirstPageFolder/FirstPage.jsx
+++ src/FirstPageFolder/FirstPage.jsx
@@ -1,9 +1,41 @@
 import './FirstPage.css'
 import checkImage from '../assets/heartimage.webp'
+import sahilHeadShot from '../assets/aischeadshot.jpg'
+
+function Card(props) {
+    return (
+        <div className="Card">
+            <img className="headshot" src={props.headshot}></img>
+            <h3 className="name">{props.name}</h3>
+            <p className="role">{props.role}</p>
+        </div>
+    );
+}
 
 const FirstPage = () => {
     return (
-        <div>
+        <div className="FirstPage">
+
+            <div className="title-div">
+                <h1 className="title">Heart Disease Predictor</h1>
+                <h3 className="subtitle">Team Heart Attack</h3>
+            </div>
+
+            <div>
+
+                <div className="FirstRow">
+                    <Card name="Sahil" role="Project Manager" headshot={sahilHeadShot}/>
+                    
+                </div>
+
+                <div className="SecondRow">
+                    <Card name="Brian" role="Technical Associate" headshot={checkImage}></Card>
+                </div>
+
+            
+            </div>
+
+
 
         </div>
     );
