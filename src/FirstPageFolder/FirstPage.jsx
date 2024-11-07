7c93665bc18a41044e155e7968468d96125a1936
Jamie Wong <jamiewong@Jamies-MacBook-Pro-2.local>
2024-11-06 18:43:35 -0800

    jamie

--- src/FirstPageFolder/FirstPage.jsx
+++ src/FirstPageFolder/FirstPage.jsx
@@ -1,11 +1,36 @@
 import './FirstPage.css'
 import checkImage from '../assets/heartimage.webp'
+import jamieHeadshot from '../assets/IMG_1142.png'
+
+function Card(props) {
+    return (
+        <div className="Card">
+            <img className="headshot" src={props.headshot}></img>
+            <h3 className="name">{props.name}</h3>
+            <p className="role">{props.role}</p>
+        </div>
+    );
+}
 
 const FirstPage = () => {
     return (
-        <div>
+        <div className="FirstPage">
+            <div className="title-div">
+                <h1 className="title">Heart Disease Predictor</h1>
+                <h3 className="subtitle">Team Heart Attack</h3>
+            </div>
 
+            <div>
+                <div className="FirstRow">
+                    <Card name="Jamie" role = "Technical Associate" headshot={checkImage}></Card>
+                </div>
+                
+                <div className="SecondRow">
+
+                </div>
+            </div>
         </div>
+
     );
 }
 
